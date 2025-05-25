d432ddcf0099125bdd3fec45fc4221b7e576163b
x88-code <xgh6695@gmail.com>
2025-05-25 15:48:01 +0300

    Salon Rewards App

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = "mongodb://localhost:27017/loyalty_rewards";
 
 app.use(cors());
